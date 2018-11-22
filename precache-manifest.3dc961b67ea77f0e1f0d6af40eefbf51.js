12a85cb9d6a4b7de116539878bdb0e60db6adbed
jenno57 <ptj2257@hotmail.com>
2018-11-22 12:51:26 +0000

    Updates

---
+++ precache-manifest.3dc961b67ea77f0e1f0d6af40eefbf51.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "72aa82900d090390d8dc",
+    "url": "/contactmanager/static/js/runtime~main.72aa8290.js"
+  },
+  {
+    "revision": "387ddc043da2969f4aa3",
+    "url": "/contactmanager/static/js/main.387ddc04.chunk.js"
+  },
+  {
+    "revision": "3c12492922852dc21010",
+    "url": "/contactmanager/static/js/1.3c124929.chunk.js"
+  },
+  {
+    "revision": "387ddc043da2969f4aa3",
+    "url": "/contactmanager/static/css/main.5bf5a46e.chunk.css"
+  },
+  {
+    "revision": "3c12492922852dc21010",
+    "url": "/contactmanager/static/css/1.4d1aa0e0.chunk.css"
+  },
+  {
+    "revision": "05edb90000e28517558871c0def3e7ad",
+    "url": "/contactmanager/index.html"
+  }
+];
